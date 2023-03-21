9c7ab0032bc13e51359a8997345a8b84b4e17bef
Mahad444 <mahadsaid95@icloud.com>
2023-03-21 16:19:15 +0300

    Starting to Learn VueJs

---
+++ index.vue
@@ -0,0 +1,16 @@
+// Usage of VUe``
+Vue.createApp({
+    data:function() {
+        return {
+            goal: '',
+            goals:[],
+        }
+    },
+    methods: {
+        addGoal() {
+            console.log(this.goal)
+            this.goals.push(this.goal)
+            this.goal = ''
+        }
+    },
+}).mount('#app')
